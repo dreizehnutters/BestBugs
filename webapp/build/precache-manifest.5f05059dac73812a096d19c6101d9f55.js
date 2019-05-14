6a1470b004c29bc5c22bf1e558fbf324263e7b28
Opuchlich, Paul <paul.opuchlich@sap.com>
2019-05-14 13:51:34 +0200

    build folder

---
+++ webapp/build/precache-manifest.5f05059dac73812a096d19c6101d9f55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f006d21a84fbffbba83",
+    "url": "./static/css/main.f029c749.chunk.css"
+  },
+  {
+    "revision": "0f006d21a84fbffbba83",
+    "url": "./static/js/main.0f006d21.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "76a6fbbecee1c3ebc08e",
+    "url": "./static/js/2.76a6fbbe.chunk.js"
+  },
+  {
+    "revision": "ab454e17b71fcd4b6aae5aa8f0959de5",
+    "url": "./index.html"
+  }
+];
