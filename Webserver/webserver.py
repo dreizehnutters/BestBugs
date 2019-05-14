468f323ca30a4143e687baa79a7cf357984c0e72
Rene Heine <rene.heine98@gmail.com>
2019-05-14 11:43:01 +0200

    webserver test

--- Webserver/webserver.py
+++ Webserver/webserver.py
@@ -1,1 +1,30 @@
 from flask import Flask
+import json
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/current_data', method = 'GET')
+def current_data():
+    current_temp = 25.2
+    current_moisture =
+
+
+@app.route('/historical_data')
+def historical_data():
+    pass
+
+
+@app.route('/feed')
+def feed():
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='8000')
